f793773a65ad6eeda0d5f856d2e5236b74508ab3
Felixwijaya04 <felixwijaya2004@gmail.com>
2024-01-29 14:01:54 +0700

    add material
    movement player dan enemy ngebug

--- revisi/Assets/Scripts/EnemyScript.cs
+++ revisi/Assets/Scripts/EnemyScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     private float walkspeed = 6f;
+    private float jumpingpower = 6f;
 
     [SerializeField] private Rigidbody2D rb;
     void Update()
@@ -13,4 +14,12 @@ public class EnemyMovement : MonoBehaviour
         Physics2D.IgnoreLayerCollision(6, 7);
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacles"))
+        {
+            Debug.Log("1");
+            rb.velocity = new Vector2(walkspeed, jumpingpower);
+        }
+    }
 }
